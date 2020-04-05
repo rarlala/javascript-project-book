09f5a88cfbd3aee38b101715ca2919db4d6ca3cc
JEONGSUJI <wtw9611@gmail.com>
2020-04-05 11:23:45 +0900

    계산기 프로젝트 - 자바스크립트 코드를 제이쿼리로 코드를 변경해 봄

--- calculator/index.js
+++ calculator/index.js
@@ -1,59 +1,62 @@
-let $inp = document.forms['cal'];
-let $input = $inp.getElementsByTagName('input');
-let $cls_btn = document.getElementsByClassName('cls_btn')[0];
-let $result_btn = document.getElementsByClassName('result_btn')[0];
+// let $inp = document.forms['cal'];
+// let $input = $inp.getElementsByTagName('input');
+// let $cls_btn = document.getElementsByClassName('cls_btn')[0];
+// let $result_btn = document.getElementsByClassName('result_btn')[0];
+
+// jquery로 변수선언
+let $inp = $('form[name=cal]');
+let $input = $inp.find('input');
+let $cls_btn = $('.cls_btn');
+let $result_btn = $inp.find('input[name=result]');
+let $result = $inp.find('input[name=result]');
 
 // 계산기 입력 처리 함수
 
 function calc(value) {
     // 입력이 들어가면 0을 지운다.
-    if ($inp['result'].value == 0) {
-        $inp['result'].value = '';
+    if ($result.val() == 0) {
+        $result.val('');
     }
 
     // 입력 값을 결과창에 출력한다.
-    $inp['result'].value += value;
+    var val = $result.val() + value;
+    $result.val(val);
 }
 
 // 계산기 초기화 clear
 
 function clr() {
-    $inp['result'].value = 0;
+    $result.value(0);
 }
 
 // 계산 결과 처리 함수
 
 function my_result() {
-    var result = document.forms['cal']['result'];
-
-    var calc = eval(result.value);
-
-    $inp['result'].value = calc;
+    var calc = eval($result.val());
+    $result.val(calc);
 }
 
 // 이벤트 핸들러
 
 // 숫자 및 사칙 연산 버튼
 
-for (let i = 0; i < $input.length; i++) {
-    if ($input[i].value != '=' && $input[i].value != 'clear') {
-        $input[i].onclick = function() {
-            calc(this.value);
-        }
+$('input').click(function() {
+    var $input_value = $(this).val();
+    if ($input.value != '=' && $input.value != 'clear') {
+        calc(this.value);
     }
-}
+});
 
 // 초기화 버튼
-$cls_btn.onclick = function() {
+$('.cls_btn').click(function() {
     clr();
-}
+});
 
 // 결과 버튼
-$result_btn.onclick = function() {
+$('.result_btn').click(function() {
     try {
         my_result();
     } catch (error) {
-        var $result = $inp['result'];
-        $result.value = '입력 오류';
+        $result.val('입력 오류');
     }
-}
+})
